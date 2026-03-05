4fcad4e43b37b521b3ad82f70ae2bea7fc5f7758
Juanes Urrego <024juanurrego@gmail.com>
2026-03-04 23:24:48 -0500

    fead: agregar main del proyecto

---
+++ src/Principal.java
@@ -0,0 +1,10 @@
+import gui.VentanaMenu;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        VentanaMenu menu = new VentanaMenu();
+        menu.setVisible(true);
+    }
+}
